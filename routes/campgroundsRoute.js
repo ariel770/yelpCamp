9af77286cade6090055f77dae5794cef81dad708
pinjas <pinjas31@gmail.com>
2020-07-29 12:22:36 +0300

    change back to express-session

--- routes/campgroundsRoute.js
+++ routes/campgroundsRoute.js
@@ -109,9 +109,8 @@ route.post("/", middlewhere.isLoggedIn, function (req, res) {
         kettle: req.body.kettle
     }
     
-    console.log("================================================= "+req.body.location);
     geocoder.geocode(req.body.location, function (err, data) {
-        console.log("=========================   ==  == == = = =  "+data);
+       
         if (err || !data.length) {
           req.flash('error', 'Invalid address');
           return res.redirect('back');
